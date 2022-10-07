6810c9e8c643834d91cd6ab4bf8c47e6216c8f32
simon <283736566@qq.com>
2022-10-08 00:01:10 +0800

    Demo: add spring securety module

---
+++ src/main/java/com/simon/stunningfiesta/model/Role.java
@@ -0,0 +1,30 @@
+package com.simon.stunningfiesta.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+}
